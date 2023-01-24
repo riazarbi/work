d259655d3d71edcc08618ce4b8385ba59cf47063
Riaz Arbi <riazarbi@gmail.com>
2023-01-24 08:02:07 +0000

    add totals code

--- scripts/create_dataset.R
+++ scripts/create_dataset.R
@@ -5,20 +5,35 @@ library(tidyr)
 library(stringr)
 library(snakecase)
 
-work <- read_delim("task_logging/work.txt", delim=";")
-payments <- read_delim("task_logging/payments.txt", delim=";")
+work_log <- ifelse(file.exists("task_logging/work.txt"), 
+               "task_logging/work.txt", 
+               "examples/work.txt")
+
+payments_log <- ifelse(file.exists("task_logging/payments.txt"), 
+                   "task_logging/payments.txt", 
+                   "examples/payments.txt")
+
+work <- read_delim(work_log, delim=";")
+payments <- read_delim(payments_log, delim=";")
 
 work |> 
   mutate(across(everything(), str_trim)) |>
-  mutate(typical_duration_min = as.numeric(typical_duration_min),
-         context = to_upper_camel_case(context)) |>
-  mutate(hours = typical_duration_min / 60) |> 
-  mutate(rate = as.numeric(rate)) |>
+  mutate(Duration = as.numeric(Duration),
+         Project = to_upper_camel_case(Project)) |>
+  mutate(Hours = Duration / 60) |> 
+  mutate(Rate = as.numeric(Rate)) |>
   mutate(Timestamp = as_datetime(Timestamp)) |>
-  mutate(Amount = rate * hours) |>
-  group_by(context) |>
-  summarise(Hours = sum(hours),
+  mutate(Amount = Rate * Hours) |>
+  group_by(Project) |>
+  summarise(Hours = sum(Hours),
             Total = sum(Amount))
 
-
-         
+payments |> 
+  mutate(across(everything(), str_trim)) |>
+  mutate(Amount = as.numeric(Amount),
+         Project = to_upper_camel_case(Project),
+         Account = to_upper_camel_case(Account),
+         `Record Date` = as_datetime(`Record Date`),
+         `Received Date` = as_datetime(`Received Date`)) |>
+  group_by(Project) |>
+  summarise(Amount = sum(Amount))
